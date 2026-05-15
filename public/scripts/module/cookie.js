2d79710f6804fd55175be731197fc4a00b89904d
Sharky <sharky@wireless.fish>
2026-05-15 09:02:52 +0200

    doing things

---
+++ public/scripts/module/cookie.js
@@ -0,0 +1,31 @@
+// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+export function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/" + ";SameSite=None;Secure";
+}
+
+export function setCookieMinutes(name, value, minutes) {
+  var expires = "";
+  if (minutes) {
+    var date = new Date();
+    date.setTime(date.getTime() + minutes * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/" + ";SameSite=None;Secure";
+}
+
+export function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
---
+++ public/scripts/statuscafe.js
@@ -0,0 +1,36 @@
+import { setCookieMinutes, getCookie } from "./module/cookie.js";
+
+const cookie_name = "scafe";
+const cookie_minutes = 5;
+
+function getSCafeCookie() {
+  return JSON.parse(getCookie(cookie_name));
+}
+
+async function getSCafeData() {
+  let cookie = getSCafeCookie();
+
+  if (cookie) {
+    return cookie;
+  }
+
+  let response = await fetch("https://status.cafe/users/sharky/status.json");
+  response = await response.json();
+
+  let data = {
+    author: response.author,
+    timeAgo: response.timeAgo,
+    content: response.content,
+  };
+
+  setCookieMinutes(cookie_name, JSON.stringify(data), cookie_minutes);
+
+  return data;
+}
+
+let data = await getSCafeData();
+
+document.getElementById("statuscafe-username").innerHTML =
+  '<a href="https://status.cafe/users/sharky" target="_blank">' + data.author + "</a> ";
+document.getElementById("statuscafe-ago").textContent = data.timeAgo;
+document.getElementById("statuscafe-content").textContent = `"${data.content}"`;
