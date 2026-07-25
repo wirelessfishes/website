72d04d38ee088f64978dc18247a475f59c01c695
Sharky <sharky@wireless.fish>
2026-07-25 12:28:34 +0200

    Style changes and cleanup!

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,12 +2,16 @@ export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
 
-  eleventyConfig.addPassthroughCopy({ "public": "/" });
+  eleventyConfig.addPassthroughCopy({ public: "/" });
   eleventyConfig.addPassthroughCopy({ "LICENSE.txt": "LICENSE.txt" });
   eleventyConfig.addPassthroughCopy({ "MIT-LICENSE.txt": "MIT-LICENSE.txt" });
 
   eleventyConfig.addFilter("utcDate", (date) => {
     return new Date(date).toLocaleDateString("en-CA");
+  });
+
+  eleventyConfig.addCollection("shark_pages", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/shark/*.md");
   });
 
   return {
