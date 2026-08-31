fe1c8a3b5706fad428154ed9aacd5aae335f21fb
Sharky <sharky@wireless.fish>
2026-08-31 17:02:57 +0200

    added floating sharks!!!

---
+++ src/components/FloatingImg.svelte
@@ -0,0 +1,18 @@
+<script>
+  const { src, alt, top, left, width } = $props();
+</script>
+
+<div class="wrapper" style:width>
+  <img {alt} {src} style:top style:left />
+</div>
+
+<style>
+  .wrapper {
+    position: relative;
+    pointer-events: none;
+  }
+
+  img {
+    position: absolute;
+  }
+</style>
